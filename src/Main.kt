ef96531dfbed49c29c9b90c7b08183d5739fba37
diegoip <diegoip38@educastur.es>
2024-11-25 18:03:13 +0100

    proyecto2

--- src/Main.kt
+++ src/Main.kt
@@ -2,16 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
-    for (i in 1..100){
-        if(i%3 == 0 && i%5 == 0) {
-            println("quintri")
-        }else if(i%3 == 0){
-            println("tri")
-        }else if(i%5 == 0){
-            println("quin")
-        }else println(i)
 
-    }
 
 
 }
