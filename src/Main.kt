5ba9276ac5e53d01e0c8334733ae9585fb66f484
diegoip <diegoip38@educastur.es>
2024-11-27 18:37:26 +0100

    proyecto4_2

--- src/Main.kt
+++ src/Main.kt
@@ -2,35 +2,55 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
-    val a1 = arrayListOf<String>("correr","saltar","correr","saltar","correr")
-    val a2 = arrayListOf<String>("correr", "correr", "correr", "saltar", "correr")
-    val a3 = arrayListOf<String>("correr", "correr", "saltar", "saltar", "correr")
-    val a4 = arrayListOf<String>("correr", "correr", "saltar", "saltar", "correr")
-    val a5 = arrayListOf<String>("correr", "saltar", "correr", "saltar")
-    val a6 = arrayListOf<String>("correr", "saltar", "correr", "saltar", "correr", "saltar", "correr")
-    val a7 = arrayListOf<String>("saltar", "saltar", "saltar", "saltar", "saltar")
-    val a8 = arrayListOf<String>("saltar", "saltar", "saltar", "saltar", "saltar")
+    val a1 = listOf("correr","saltar","correr","correr")
+    val a2 = listOf("correr","saltar","correr")
 
+    val c1 : String = "_|_"
+    val c2 : String = "_||"
 
-    val c1:String = "_|_|_"
-
+    println(Carrera(a1,c1))
+    println(Carrera(a2,c1))
+    println(Carrera(a2,c2))
 
 }
 
-private fun Carrera(a:Array<String>,c:String){
+private fun Carrera(a:List<String>,c:String):Boolean{
+
+
+
+    val cm = c.toMutableList()
+
 
     for (i in a.indices){
-        if(a[i]=="correr" && c[i] == '_'){
-            print("_")
-        }else if(a[i]=="correr" && c[i] == '|'){
-            print("/")
-        }else if(a[i]=="saltar" && c[i] == '|'){
-            print("|")
-        }else if(a[i]=="saltar" && c[i] == '_'){
-            print("x")
-        }else print("?")
 
+
+        when (a[i]){
+            "correr" ->{
+                if( cm[i] == '|'){
+                    cm[i]='/'
+                    print(cm)
+                    return false
+                }
+            }
+            "saltar"-> {
+                if (cm[i] == '_') {
+                    cm[i] = 'x'
+                    print(cm)
+                    return false
+                }
+            }
+        }
+        if  (a.size != c.length){
+            val min = minOf(a.size , c.length)
+            for (j in 0 until min){
+                cm.add('?')
+                print(cm)
+                return false
+            }
+        }
     }
+    print(cm)
+    return true
 }
 
 
